824962d77239216c69bb2d5a73d314b13e044cd7
Krystian <krys225@gmail.com>
2014-05-29 00:18:56 +0200

    Added new preferences to preferences screen

---
+++ phone-sms-gate-android/SmsGate/src/com/example/smsgate/EditContactsListActivity.java
@@ -0,0 +1,14 @@
+package com.example.smsgate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditContactsListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contacts_list_edit);
+	}
+
+}
