124f396e42c1cc717af371ad6e1ab4681ea57e68
Krystian <krys225@gmail.com>
2014-05-30 23:51:27 +0200

    Added saving contacts to and reading from file, implemented addition of new contacts

--- phone-sms-gate-android/SmsGate/src/com/example/smsgate/EditContactsListActivity.java
+++ phone-sms-gate-android/SmsGate/src/com/example/smsgate/EditContactsListActivity.java
@@ -10,20 +10,46 @@ import java.io.OutputStreamWriter;
 import java.util.Vector;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+
+class Contact
+{
+	String name;
+	String phoneNumber;
+	
+	Contact() { name = ""; phoneNumber = ""; }
+	
+	Contact(String name, String phoneNumber) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		return name + ", " + phoneNumber;
+	}
+	
+	
+}
 
 public class EditContactsListActivity extends Activity {
 
 	static final String TAG = "EditContactsActivity";
 	
-	Vector<String> values;
-	ArrayAdapter<String> aa;
+	Vector<Contact> values;
+	ArrayAdapter<Contact> aa;	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +57,8 @@ public class EditContactsListActivity extends Activity {
 		setContentView(R.layout.activity_contacts_list_edit);
 
 		ListView lv = (ListView)findViewById(R.id.contactsList);
-		values = new Vector<String>();
-		aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+		values = new Vector<Contact>();
+		aa = new ArrayAdapter<Contact>(this, android.R.layout.simple_list_item_1, values);
 		lv.setAdapter(aa);
 	}
 	
@@ -42,10 +68,10 @@ public class EditContactsListActivity extends Activity {
 			FileInputStream fis = openFileInput("contacts.dat");
 			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
 			
-			String buf;
+			String buf1, buf2;
 			values.clear();
-			while ((buf = br.readLine()) != null)
-				values.add(buf);
+			while (((buf1 = br.readLine()) != null) && ((buf2 = br.readLine()) != null))
+				values.add(new Contact(buf1, buf2));
 			
 			br.close();
 			fis.close();
@@ -64,8 +90,10 @@ public class EditContactsListActivity extends Activity {
 			FileOutputStream fos = openFileOutput("contacts.dat", Context.MODE_PRIVATE);
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
 			
-			for (String s : values) {
-				bw.write(s);
+			for (Contact s : values) {
+				bw.write(s.name);
+				bw.newLine();
+				bw.write(s.phoneNumber);
 				bw.newLine();
 			}
 					
@@ -90,9 +118,38 @@ public class EditContactsListActivity extends Activity {
 	    // Handle presses on the action bar items
 	    switch (item.getItemId()) {
 	        case R.id.contacts_list_add:
-	        	values.add("fourth");
-	        	aa.notifyDataSetChanged();
+	        	LayoutInflater inflater = getLayoutInflater();
+	        	View v = inflater.inflate(R.layout.edit_contacts_dlg, null);
+	        	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	        	
+	        	builder.setTitle("Dodaj kontakt");
+	        	builder.setView(v);
+	        	
+	        	final EditText name = (EditText) v.findViewById(R.id.eName);
+	        	final EditText number = (EditText) v.findViewById(R.id.ePhoneNumber);
+	        	
+	        	builder.setPositiveButton(R.string.edit_contacts_dlg_ok, new OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						values.add(new Contact(name.getText().toString(), number.getText().toString()));
+						aa.notifyDataSetChanged();
+					}
+				});
+	        	
+	        	builder.setNegativeButton(R.string.edit_contacts_dlg_cancel, new OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						
+						
+					}
+				});
+	        	
+	        	AlertDialog dialog = builder.create();
+	        	dialog.show();
 	            return true;
+	            
 	        default:
 	            return super.onOptionsItemSelected(item);
 	    }
